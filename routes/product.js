a00ed81f7c34d9d8323d2d9d2e09bd17a6b06379
trannguyen33398 <trannguyen3398@gmail.com>
2019-04-20 20:48:07 +0700

    cc

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-var router = Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('product', { title: 'Express' });
+  res.render('product');
 
 });
 
-export default router;
+module.exports = router;
