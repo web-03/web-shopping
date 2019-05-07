9a43b70e6ead59e83470f6d5618beacff8080626
trannguyen33398 <44741820+trannguyen33398@users.noreply.github.com>
2019-05-07 11:22:35 +0700

    update product online db

--- controller/product.js
+++ controller/product.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var con = require('./../config/key');
 
-var product = function(id, name,price,quantity, description, categoryId, status){
+var product = function(id, name,price,quantity, description, categoryId,image, status){
   this.id = id;
   this.name = name;
   this.price = price;
   this.quantity = quantity;
   this.description = description;
   this.categoryId = categoryId;
+  this.image = image;
   this.status = status;
 }
 var productsAll = [];
@@ -17,12 +18,12 @@ var productsAll = [];
 
 /* GET home page. */
 router.getIndex = (req, res, next) => {
-
+  productsAll = [];
   con.query('select * from products', function (err, rows, fields) {
     if (err) throw err
   
     rows.forEach(element => {
-      var x = new product(element.id, element.name, element.price,element.quantity, element.detail,element.id_category, element.status);
+      var x = new product(element.id, element.name, element.price,element.quantity, element.detail,element.id_category,element.image, element.status);
       productsAll.push(x);
     })
     res.render('product/product',{products : productsAll});
