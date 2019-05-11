2b8eb02bfae8ed91ab5d1241082f19ff8bb33c9a
trannguyen33398 <trannguyen3398@gmail.com>
2019-05-11 23:22:41 +0700

    update controller home.js & index.ejs

--- controller/home.js
+++ controller/home.js
@@ -14,6 +14,21 @@ var product = function(id, name,price,quantity, description, categoryId,image, s
   this.status = status;
 }
 var productsAll = [];
+var category = function(id, name, status, description){
+  this.id = id;
+  this.name = name;
+  this.status = status;
+  this.description = description;
+}
+var categoriesAll = [];
+con.query('select * from categories', function (err, rows, fields) {
+  if (err) throw err
+
+  rows.forEach(element => {
+    var x = new category(element.id, element.name, element.status, element.description);
+    categoriesAll.push(x);
+  })
+});
 con.query('select * from products', function (err, rows, fields) {
   if (err) throw err
 
@@ -25,7 +40,7 @@ con.query('select * from products', function (err, rows, fields) {
 });
 /* GET home page. */
 router.getIndex = (req, res, next) => {
-  res.render('home/index',{products : productsAll});
+  res.render('home/index',{products : productsAll,categories : categoriesAll});
 };
 
 module.exports = router;
