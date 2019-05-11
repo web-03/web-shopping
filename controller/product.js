235c2ef671f63a3dff2e6acd63708a01a594d4a2
trannguyen33398 <44741820+trannguyen33398@users.noreply.github.com>
2019-05-11 22:02:39 +0700

    update category

--- controller/product.js
+++ controller/product.js
@@ -14,7 +14,22 @@ var product = function(id, name,price,quantity, description, categoryId,image, s
 }
 var productsAll = [];
 
+var category = function(id, name, status, description){
+  this.id = id;
+  this.name = name;
+  this.status = status;
+  this.description = description;
+}
+var categoriesAll = [];
 
+con.query('select * from categories', function (err, rows, fields) {
+  if (err) throw err
+
+  rows.forEach(element => {
+    var x = new category(element.id, element.name, element.status, element.description);
+    categoriesAll.push(x);
+  })
+});
 
 /* GET home page. */
 router.getIndex = (req, res, next) => {
@@ -26,7 +41,7 @@ router.getIndex = (req, res, next) => {
       var x = new product(element.id, element.name, element.price,element.quantity, element.detail,element.id_category,element.image, element.status);
       productsAll.push(x);
     })
-    res.render('product/product',{products : productsAll});
+    res.render('product/product',{products : productsAll, categories : categoriesAll});
   });
 };
 
